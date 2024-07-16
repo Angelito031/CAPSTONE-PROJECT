b45d7658babc433ae8993545bac493bd4fe338be
Heart Sarmiento <heart.sarmiento0925@gmail.com>
2024-07-16 22:25:41 +0800

    UPDATE DATABASE

--- server/Utils/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    message: "An error has occurred on the server",
-    error: err.message,
-  });
-};
-
-module.exports = errorHandler;
